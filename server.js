31c7f8fda620e287b27d0e90cf80dfbcbc6116ca
HG_ORO <h.g.orozco@gmail.com>
2019-04-09 12:40:43 -0700

    minor syntax bug fixed in index and server.js

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.set("view engine", "handlebars");
 app.use(routes);
 
 // Launch Server ==============================================
-//listen on port 3000
+//listen on port 3001
 
 db.sequelize.sync().then(function(){
     app.listen(PORT, function(){
         console.log("🌎  ==> Server now listening on PORT:" + PORT);
-    })
-})
+    });
+});
